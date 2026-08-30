e04a89e0e44de3aca714e1f46473b3e1eea74362
higor8951-hub <higor8951@gmail.com>
2026-08-30 14:22:38 -0400

    Atualiza cache offline para mapa operacional vetorial

--- docs/brigada/sw.js
+++ docs/brigada/sw.js
@@ -1,14 +1,10 @@
-const C='brigada-adm-offline-v9-mapa-nitido';
+const C='brigada-adm-offline-v10-mapa-operacional';
 const A=[
   './','./index.html','./manifest.webmanifest','./icon.svg',
   './areas1.js','./areas2.js','./areas3.js','./areas4.js','./scenarios.js','./quick.js',
-  // mapa de contingência
-  './map1.txt','./map2.txt','./map3.txt','./map4.txt',
-  // mapa técnico de alta nitidez
-  './clear01.txt','./clear02.txt','./clear03.txt','./clear04.txt','./clear05.txt',
-  './clear06.txt','./clear07.txt','./clear08.txt','./clear09.txt','./clear10.txt','./clear11.txt',
-  './clear12a.txt','./clear12b.txt','./clear13.txt','./clear14.txt','./clear15.txt',
-  './clear16.txt','./clear17.txt','./clear18.txt','./clear19.txt','./clear20.txt'
+  './mapa-operacional.svg',
+  // mapa técnico de contingência
+  './map1.txt','./map2.txt','./map3.txt','./map4.txt'
 ];
 self.addEventListener('install',e=>e.waitUntil(
   caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())
